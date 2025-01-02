e05d2965f6f3f02b071b37eb6bffa481e5f35f40
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-02 10:09:51 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
-// app/layout.tsx
-import React from 'react';
-import { ClerkProvider } from '@clerk/nextjs';
-import './globals.css';
-import AuthenticatedComponent from '@/app/components/AuthenticatedComponent';
-import NavBar from '@/app/components/NavBar';
-
-export default function RootLayout({ children }: any) {
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en"> {/* Ensure you have the <html> tag with the appropriate attributes */}
-      <body> {/* Ensure you have the <body> tag */}
-        <ClerkProvider >
-          <div> {/* Wrapper div for your content */}
-            <header>
-              <NavBar />
-            </header>
-            <main>
-              {/* <AuthenticatedComponent> */}
-              {children}
-              {/* </AuthenticatedComponent> */}
-            </main>
-          </div>
-        </ClerkProvider>
-      </body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
